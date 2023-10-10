b297d945fad49369aef94633046da5c6dceb5635
Rajan-Metaliya <rajanmetaliya77@gmail.com>
2023-10-10 14:48:04 +0530

    Updated pubspec

--- workshop_flutter/lib/app/my_app.dart
+++ workshop_flutter/lib/app/my_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:workshop_flutter/feature/splash/splash_screen.dart';
+import 'package:workshop_flutter/feature/splash/ui/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
